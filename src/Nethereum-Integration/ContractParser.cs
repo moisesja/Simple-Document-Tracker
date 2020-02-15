ac0fddcd713c3e95f3df74ed776276fab3881ae6
moises <moisesj@outlook.com>
2020-02-14 22:00:38 -0500

    Able to integrate nethereum

---
+++ src/Nethereum-Integration/ContractParser.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Nethereum_Integration
+{
+    public class ContractMeta
+    {
+        private readonly JObject _jObject;
+
+        public ContractMeta(string path)
+        {
+            _jObject = JObject.Parse(File.ReadAllText(path));
+        }
+
+        public string Abi => _jObject.GetValue("abi").ToString();
+    }
+}
---
+++ src/Nethereum-Integration/InquiryEntry.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Nethereum_Integration
+{
+    public class InquiryEntry
+    {
+        public InquiryEntry()
+        {
+        }
+    }
+}
